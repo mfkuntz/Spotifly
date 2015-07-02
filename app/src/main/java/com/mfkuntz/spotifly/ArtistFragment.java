06192d4f9b87a81443691efbd857f36a7450b7cc
Matthew Kuntz <matt@mfkuntz.com>
2015-07-01 22:34:11 -0400

    Search Spotify Api for artists. Might want to add wildcard?

--- app/src/main/java/com/mfkuntz/spotifly/ArtistFragment.java
+++ app/src/main/java/com/mfkuntz/spotifly/ArtistFragment.java
@@ -3,16 +3,24 @@ package com.mfkuntz.spotifly;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import kaaes.spotify.webapi.android.SpotifyApi;
 import kaaes.spotify.webapi.android.SpotifyService;
+import kaaes.spotify.webapi.android.models.Artist;
+import kaaes.spotify.webapi.android.models.Artists;
 import kaaes.spotify.webapi.android.models.ArtistsPager;
 import retrofit.Callback;
 import retrofit.RetrofitError;
@@ -33,7 +41,8 @@ public class ArtistFragment extends Fragment {
 
     public void onStart(){
         super.onStart();
-        updateArtist();
+
+//        updateArtist("Coldplay");
     }
 
     @Override
@@ -42,62 +51,73 @@ public class ArtistFragment extends Fragment {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
-        ArrayList<String> temp = new ArrayList<>();
-        temp.add("Test 1");
-        temp.add("Test 2");
-        temp.add("Test 3");
 
-        artistAdapter = new ArrayAdapter<String>(
+
+
+        artistAdapter = new ArrayAdapter<>(
                 getActivity(),
                 R.layout.list_item_artist,
                 R.id.list_item_artist_textview,
-                temp
+                new ArrayList<String>()
         );
 
         ListView list = (ListView) rootView.findViewById(R.id.artist_list);
         list.setAdapter(artistAdapter);
 
+
+        EditText textbox = (EditText) rootView.findViewById(R.id.artist_text);
+        textbox.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                updateArtist(s.toString());
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
         return rootView;
     }
 
-    private void updateArtist(){
-        GetArtistDataTask task = new GetArtistDataTask();
+    private void updateArtist(String artistName){
+        final String LOG_TAG = this.getClass().getSimpleName();
 
-        task.execute("Coldplay");
+        SpotifyApi api = new SpotifyApi();
+        SpotifyService service = api.getService();
+
+        service.searchArtists(artistName, new Callback<ArtistsPager>() {
+            @Override
+            public void success(ArtistsPager artistsPager, Response response) {
+                ArrayList<String> artistNames = new ArrayList<>();
+
+                for(Artist artist : artistsPager.artists.items){
+                    artistNames.add(artist.name);
+                }
+                artistAdapter.clear();
+                artistAdapter.addAll(artistNames);
+
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+                Log.e(LOG_TAG, "Could not load search");
+            }
+        });
     }
 
-    class GetArtistDataTask extends AsyncTask<String, Void, String[]> {
 
 
-        @Override
-        protected String[] doInBackground(String... params) {
-
-            SpotifyApi api = new SpotifyApi();
-            SpotifyService service = api.getService();
-
-            service.searchArtists(params[0], new Callback<ArtistsPager>() {
-                @Override
-                public void success(ArtistsPager artistsPager, Response response) {
-                    artistsPager.artists.items
-                }
-
-                @Override
-                public void failure(RetrofitError error) {
-
-                }
-            });
-
-
-            return new String[0];
-        }
-
-        protected void onPostExecute(String[] results){
-            artistAdapter.clear();
-            artistAdapter.addAll(results);
-
-            super.onPostExecute(results);
-        }
-    }
 
 }
+
+
 
