ad6d87dba125eedafb95cd608bdf83794a1dbc67
Matthew Kuntz <matt@mfkuntz.com>
2015-07-02 13:42:14 -0400

    Get Artist image. May want to manually check each image url size to find the smallest image to download.

--- app/src/main/java/com/mfkuntz/spotifly/ArtistListAdapter.java
+++ app/src/main/java/com/mfkuntz/spotifly/ArtistListAdapter.java
@@ -2,11 +2,15 @@ package com.mfkuntz.spotifly;
 
 import android.content.ClipData;
 import android.content.Context;
+import android.media.Image;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -35,8 +39,20 @@ public class ArtistListAdapter extends ArrayAdapter<Artist> {
             TextView temp = (TextView) v.findViewById(R.id.list_item_artist_textview);
             temp.setText(artist.name);
 
-            temp = (TextView) v.findViewById(R.id.list_item_artist_counter);
-            temp.setText(Integer.toString(position));
+
+            ImageView image = (ImageView) v.findViewById(R.id.list_item_artist_image);
+
+            if (artist.images.size() == 0){
+                return v;
+            }
+
+            String imgUrl = artist.images.get(artist.images.size() - 1).url;
+
+            Picasso.with(getContext())
+                    .load(imgUrl)
+                    .resize(45, 45)
+                    .centerCrop()
+                    .into(image);
 
         }
 
